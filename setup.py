60d395b1a064d407d00dc80edcca666e3a6d83e8
Sebastian Vetter <sebastian@roadside-developer.com>
2012-01-14 12:58:35 +1300

    beautified setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,36 @@
 from distutils.core import setup
-import sys
 
 import colourlovers 
 
 setup(
-    name='python-colourlovers',
-    version='0.1',
-    author='Sebastian Vetter',
-    author_email='sebastian@roadside-developer.com',
-    url='http://python-colourlovers.readthedocs.org',
-    #download_url='https://sourceforge.net/projects/py-googlemaps/files/',
-    description='An API providing access to the ColourLovers website.',
-    long_description=colourlovers.__doc__,
+    name = 'python-colourlovers',
+    version = '0.1.0',
+    author = 'Sebastian Vetter',
+    author_email = 'sebastian@roadside-developer.com',
+    url = 'http://python-colourlovers.readthedocs.org',
+
+    description = 'An API providing access to the ColourLovers website.',
+    long_description = colourlovers.__doc__,
 
     packages = [
         'colourlovers', 
     ],
 
     provides = ['colourlovers'],
-    license='GNU General Public License (GPL)',
-    classifiers=[
-        'Development Status :: 3 - Alpha',
+    license = 'GNU General Public License (GPL)',
+
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Environment :: Web Environment',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Intended Audience :: Developers',
         'Natural Language :: English',
+        'Operating System :: POSIX',
         'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
