7efc687cdc682952b1b5e48f1bf94ffd9d4a16a5
Sebastian Vetter <sebastian@roadside-developer.com>
2013-06-08 16:42:39 +1000

    Add request to required packages

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# python-colourlovers - A Python API to http://www.colourlovers.com 
+# python-colourlovers - A Python API to http://www.colourlovers.com
 # Copyright (C) 2012 Sebastian Vetter
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 
-from distutils.core import setup
+from setuptools import setup
 
 
 setup(
@@ -30,7 +30,11 @@ setup(
     long_description = open('README.rst', 'r').read(),
 
     packages = [
-        'colourlovers', 
+        'colourlovers',
+    ],
+
+    requires=[
+        'requests>=1.0',
     ],
 
     provides = ['colourlovers'],
